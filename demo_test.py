3ef4997fb551b6897a087d18939618bd4d577e29
Youzhi Luo <lyzxyxdb@gmail.com>
2019-01-06 08:43:53 +0800

    change top3 compute method

--- demo_test.py
+++ demo_test.py
@@ -10,8 +10,8 @@ def get_demo_images(filename, image_path, image_h=64, image_w=64):
     label_tensor = np.zeros([len(filelist)],dtype=np.int32)
     for i in range(len(filelist)):
         num, label = filelist[i].split(" ")
-        #img = cv2.imread(os.path.join(image_path, "{}.JPEG".format(str(num))))
-        img = cv2.imread(num)
+        img = cv2.imread(os.path.join(image_path, "{}.JPEG".format(str(num))))
+        #img = cv2.imread(num)
         img = img[:,:,::-1].astype(np.float32)
         img_tensor[i] = img.transpose(2,0,1)
         label_tensor[i] = int(label)
@@ -25,14 +25,27 @@ def demo(model_path, filename, image_path, image_h=64, image_w=64):
     top3_pred = np.zeros([images.shape[0],3],dtype=np.int32)
     
     num_accurate = 0
+    unknown_p = 1/21
+
     for i in range(images.shape[0]):
         out = net.forward(images[i].reshape(1,3,image_h,image_w)).reshape(-1)
         order = np.argsort(out)[::-1]
-        if order[0] < 0.5:
-            top3_pred[i,0] = 21
-            top3_pred[i,1:3] = order[0:2]+1
+
+        if out[order[0]] < unknown_p:
+            top3_pred[i,0] = 20
+            top3_pred[i,1:3] = order[0:2]
+        elif out[order[1]] < unknown_p:
+            top3_pred[i,0] = order[0]
+            top3_pred[i,1] = 20
+            top3_pred[i,2] = order[1]
+        elif out[order[2]] < unknown_p:
+            top3_pred[i,0:2] = order[0:2]
+            top3_pred[i,2] = 20
         else:
-            top3_pred[i,0:3] = order[0:3]+1
+            top3_pred[i] = order[0:3]
+        
+        top3_pred += 1
+
         print("Top 3 prediction: {}  {}  {} || truth: {}".format(top3_pred[i,0], top3_pred[i,1], top3_pred[i,2], labels[i]))
         if labels[i] == top3_pred[i,0] or labels[i] == top3_pred[i,1] or labels[i] == top3_pred[i,2]:
             num_accurate += 1
